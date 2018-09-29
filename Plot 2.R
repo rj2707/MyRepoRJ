626a354f00bbc2a1dd284404e195b31b36044e49
rj2707 <43604766+rj2707@users.noreply.github.com>
2018-09-29 15:27:27 +0530

    Delete Plot 2.R

--- Plot 2.R
+++
@@ -1,5 +0,0 @@
-
-#Plot no.2
-data$Global_active_power = as.numeric(as.character(data$Global_active_power))
-plot(data$Days, data$Global_active_power, type = "l",xlab = " ", ylab = "Global Active Power (kilowatts)")
-
